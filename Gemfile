350ed8aadbd4f3ec4e7baf66ed4650c8c1f586d6
Javier Rodriguez <javier.ri94@outlook.com>
2026-07-21 21:15:15 +0200

    chore: add factory bot and simplecov

--- Gemfile
+++ Gemfile
@@ -35,3 +35,8 @@ group :development, :test do
   # Omakase Ruby styling [https://github.com/rails/rubocop-rails-omakase/]
   gem "rubocop-rails-omakase", require: false
 end
+
+group :test do
+  gem "factory_bot_rails"
+  gem "simplecov", require: false
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,24 @@
+require "simplecov"
+SimpleCov.start "rails" do
+  enable_coverage :branch
+end
+
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
 module ActiveSupport
   class TestCase
-    # Run tests in parallel with specified workers
+    include FactoryBot::Syntax::Methods
+
     parallelize(workers: :number_of_processors)
 
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    fixtures :all
+    parallelize_setup do |worker|
+      SimpleCov.command_name "#{SimpleCov.command_name}-#{worker}"
+    end
 
-    # Add more helper methods to be used by all tests here...
+    parallelize_teardown do |worker|
+      SimpleCov.result
+    end
   end
 end
